3cc9cc935c1879c96598a5b547fc264fbb0d3afb
Mouzone <sunnyliu010@gmail.com>
2025-07-16 22:12:17 -0400

    Remove signup form, and add signin form to form.html for reactive change and remove signIn.js

--- utility/signIn.js
+++ utility/signIn.js
@@ -1,16 +1,15 @@
 const { createClient } = window.supabase;
-// Create a single supabase client for interacting with your database
+
 const supabase = createClient(
 	"https://mhdeybfxcebumtjpurby.supabase.co",
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oZGV5YmZ4Y2VidW10anB1cmJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MzYwMDIsImV4cCI6MjA2ODExMjAwMn0.GL78FfZ0Wgad8sHZ2sv9pVDdW03u6gYTRud-AVhl580"
 );
 
-async function signUpNewUser(email, password) {
-	const { data, error } = await supabase.auth.signUp({
+async function signInWithEmail(email, password) {
+	const { data, error } = await supabase.auth.signInWithPassword({
 		email,
 		password,
 	});
+
 	console.log(data, error);
 }
-
-signUpNewUser("sunnyliu010@gmail.com", "12wedfvb");
